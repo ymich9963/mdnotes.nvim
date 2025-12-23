cb06497e8452ff75934eeed97ae950d30ea64571
ymich9963 <gmichael834@gmail.com>
2025-12-23 16:57:20 +0000

    fix(inline_links): Make toggling links more robust
    Allow inserting inline links in NORMAL mode without selecting text. Fix
    issue with check_md_format()

--- lua/mdnotes/inline_link.lua
+++ lua/mdnotes/inline_link.lua
@@ -17,6 +17,23 @@ function M.insert()
     local line = vim.api.nvim_get_current_line()
     local selected_text = line:sub(col_start, col_end)
 
+    -- This would happen when there is no selection
+    if selected_text == "" then
+        -- Get the word under cursor and cursor position
+        local current_col = vim.fn.col('.')
+        selected_text = vim.fn.expand("<cword>")
+
+        -- Search for the word in the line and check if it's under the cursor
+        for start_pos, end_pos in line:gmatch("()" .. selected_text .. "()") do
+            start_pos = vim.fn.str2nr(start_pos)
+            end_pos = vim.fn.str2nr(end_pos)
+            if start_pos < current_col and end_pos > current_col then
+                col_start = start_pos
+                col_end = end_pos - 1
+            end
+        end
+    end
+
     -- Create a new modified line with link
     local new_line = line:sub(1, col_start - 1) .. '[' .. selected_text .. '](' .. reg .. ')' .. line:sub(col_end + 1)
 
@@ -30,7 +47,8 @@ function M.delete()
 end
 
 function M.toggle()
-    if M.check_md_format(require("mdnotes.patterns").inline_link) then
+    local check_md_format = require('mdnotes.formatting').check_md_format
+    if check_md_format(require("mdnotes.patterns").inline_link) then
         M.delete()
     else
         M.insert()
