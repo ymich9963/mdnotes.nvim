2299ebdb9df73cd722f73706c5577bf9298ba202
ymich9963 <gmichael834@gmail.com>
2026-03-12 12:53:01 +0000

    tests: setup CI workflow

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -5,7 +5,15 @@ vim.cmd([[let &rtp.=','.getcwd()]])
 if #vim.api.nvim_list_uis() == 0 then
     -- Add 'mini.test' to 'runtimepath' to be able to use 'mini.test'
     -- Assumed that 'mini.test' is cloned in a known path
-    local mini_path = vim.fs.normalize(vim.fn.stdpath('data') .. '/lazy/mini.test')
+    local mini_path
+    if vim.fn.isdirectory("deps/mini.test") == 1 then
+        -- This is used for CI
+        mini_path = "deps/mini.test"
+    else
+        -- This is used when developing
+        mini_path = vim.fs.normalize(vim.fn.stdpath('data') .. '/lazy/mini.test')
+    end
+
     vim.cmd('set rtp+=' .. mini_path)
 
     -- Set up 'mini.test'
