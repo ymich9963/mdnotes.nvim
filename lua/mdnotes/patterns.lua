cf7a7e61d794c551031b9b2d3773899095dcd404
ymich9963 <gmichael834@gmail.com>
2026-06-19 15:06:04 +0100

    fix: update patterns

--- lua/mdnotes/patterns.lua
+++ lua/mdnotes/patterns.lua
@@ -7,14 +7,16 @@ local M = {}
 ---@class MdnPatterns
 ---@field wikilink MdnPattern WikiLink pattern
 ---@field inline_link MdnPattern Inline link pattern
----@field text_uri string Text and URI from inline link pattern
 ---@field strong MdnPattern Strong format indicator pattern
 ---@field emphasis MdnPattern Emphasis format indicator pattern
 ---@field strikethrough MdnPattern Strikethrough format indicator pattern
 ---@field inline_code MdnPattern Inline code format indicator pattern
 ---@field autolink MdnPattern Autolink format indicator pattern
+---@field text_uri string Text and URI from inline link pattern
+---@field uri_title string Title from inline link URI
 ---@field uri_no_fragment string URI only pattern
 ---@field fragment string Fragment only pattern
+---@field wikilink_alias string WikiLink alias
 ---@field unordered_list string Unordered list pattern
 ---@field ordered_list string Ordered list pattern
 ---@field task string Task item pattern
@@ -22,13 +24,14 @@ local M = {}
 M = {
     wikilink = "()%[%[(.-)%]%]()",
     inline_link = "()([!]?%[[^%]]+%]%([^%)]+%))()",
-    text_uri = "%[([^%]]+)%]%((.+)%)",
-    uri_title = ".+%s\"([^\"]*)\"",
     strong = "()[%*_][%*_]([^%*_].-)[%*_][%*_]()",
     emphasis = "()[%*_]([^%*_].-)[%*_]()",
     strikethrough = "()~~(.-)~~()",
     inline_code = "()`([^`]+)`()",
     autolink = "()<(.-)>()",
+
+    text_uri = "%[([^%]]+)%]%((.+)%)",
+    uri_title = ".+%s\"([^\"]*)\"",
     uri_no_fragment = "^([^#]+).*",
     fragment = "#([^|]*)",
     wikilink_alias = ".+|([^|]+)",
